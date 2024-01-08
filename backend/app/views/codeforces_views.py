7dff99c372c1da87dfe5663164c48ecc8e0ec74b
boriskiva <boriskiva3@gmail.com>
2024-01-08 14:40:12 +0300

    making function in api

--- backend/app/views/codeforces_views.py
+++ backend/app/views/codeforces_views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -23,4 +25,19 @@ def problemset(request):
         return JsonResponse({'result': result_response}, status=200)
 
 def daily_task(request):
-    pass
+    if request.method == 'GET':
+        rating = None
+        if rating is None:
+            rating = 1400
+        codeforces_request = CodeforcesRequest()
+        response = codeforces_request.get_problemset()
+
+        # TODO check that problem is not solved
+        available_problems = []
+        for problem in response['result']['problems']:
+            try:
+                if abs(problem['rating'] - rating) <= 200:
+                    available_problems.append(problem)
+            except:
+                pass
+        return JsonResponse({'result': available_problems[random.randint(0,len(available_problems))]}, status=200)
