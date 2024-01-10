901208fea31a82adb7a5faa62158bb9bf04dfb6c
boriskiva <boriskiva3@gmail.com>
2024-01-10 09:02:46 +0300

    creating django part of topics

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -4,13 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .views.codeforces_views import problemset, daily_task
-from .views.olympiads import get_olyampiads
+from .views.olympiads_view import get_olyampiads
+from .views.topics_view import get_topics
 
 urlpatterns = [
     path('codeforces/problemset/', problemset),
     path('codeforces/daily-task/', daily_task),
 
     path('olympiads/', get_olyampiads),
+    path('topics/', get_topics),
 ]
 
 if settings.DEBUG:
