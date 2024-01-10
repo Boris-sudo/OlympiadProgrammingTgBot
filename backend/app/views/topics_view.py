202fb5cef45dfd749866980c2f0917aa04d643dc
boriskiva <boriskiva3@gmail.com>
2024-01-10 09:06:18 +0300

    creating django part of topics

--- backend/app/views/topics_view.py
+++ backend/app/views/topics_view.py
@@ -1,21 +1,19 @@
 from django.http import JsonResponse
 
-from ..models import Olympiad
-from ..models.olympiads import generate_olympiads
+from ..models import Topic, TopicArchive
+from ..models.topic import generate_topics
 
 
 def get_topics(request):
     if request.method == 'GET':
-        olympiads = Olympiad.objects.all()
-        if len(olympiads) == 0:
-            generate_olympiads()
-            olympiads = Olympiad.objects.all()
-
+        topics = TopicArchive.objects.all()
+        if len(topics) == 0:
+            generate_topics()
+            topics = TopicArchive.objects.all()
         result = []
-        for olympiad in olympiads:
-            result.append({'name': olympiad.name, 'link': olympiad.link})
-        print(result)
-        print()
-        print()
+        for topic in topics:
+            result.append({'name': topic.name, 'children': []})
+            for child in topic.children.all():
+                result[-1]['children'].append({'name': child.name, 'link': child.link})
         return JsonResponse({'result': result}, status=200)
     return JsonResponse({'result': 'failed'}, status=400)
